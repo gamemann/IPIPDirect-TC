1ca24dfde412892ac1597b9b3c6bca4abbc2fe39
Christian Deacon <christianmdeacon@gmail.com>
2024-06-30 17:18:19 -0400

    Fix compilition warnings related to offsetof().

--- src/include/common.h
+++ src/include/common.h
@@ -2,9 +2,9 @@
 #include <limits.h>
 
 #ifdef __BPF__
-#define SEC(NAME) __attribute__((section(NAME), used))
 #define likely(x) __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
+
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #define htons(x) ((__be16)___constant_swab16((x)))
 #define ntohs(x) ((__be16)___constant_swab16((x)))
@@ -15,20 +15,6 @@
 #define ntohs(X) (x)
 #define htonl(x) (x)
 #define ntohl(x) (x)
-#endif
-
-#ifdef DEBUG
-#ifndef printk
-# define printk(fmt, ...)                                      \
-    ({                                                         \
-        char ____fmt[] = fmt;                                  \
-        trace_printk(____fmt, sizeof(____fmt), ##__VA_ARGS__); \
-    })
-#endif
-#endif
-
-#ifndef offsetof
-# define offsetof(TYPE, MEMBER)	((uint16_t)&((TYPE *)0)->MEMBER)
 #endif
 #endif
 
